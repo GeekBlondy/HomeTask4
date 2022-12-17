b442e84ed1517e5e053432462bf89050bff1d152
Elena <emwatercolor@gmail.com>
2022-12-17 21:48:09 +0300

    First task done

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Генерирующий метод заполнения массива случайными величинами
-
+/*
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     // Данная строчка выделит память под массив array с кол-вом эл-ов size
@@ -34,14 +34,45 @@ int max = Convert.ToInt32(Console.ReadLine());
 // Вызываем метод, возвращаем массив
 int[] myArray = CreateRandomArray(length, min, max);
 WriteArray(myArray);
-
+*/
 
 
 // 1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // Math.Pow не использовать, изобрести метод аналогичный. Проверка Math.Pow
+
+/*
+int LikePowMath(int A, int B)
+{
+    int An = A;
+    int count = 1;
+    while (count < B)
+    {
+        An = An * A;
+        count++;
+    }
+    return An;
+}
+
+Console.Write("Imput number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int result = LikePowMath(A, B);
+Console.Write($"Power of A is: {result}");
+*/
 
 // 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // Как факториал
 
 // 3. Напишите программу, которая задаёт массив из size элементов и выводит их на экран.
 // Написать метод числами по запросу пользователя writeline, readline
+
+/*
+int[] NewArray8(int num)
+{
+    int[] array8 = new int[size];
+    int size = 8;
+
+}
+*/
